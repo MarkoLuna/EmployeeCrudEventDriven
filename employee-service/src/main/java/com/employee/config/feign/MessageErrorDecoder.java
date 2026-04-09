d09f6489d49232f3b3ca1f0f7fbe3c0e211ea327
Marcos Luna <josemarcosluna9@gmail.com>
2026-04-09 17:19:53 -0600

    refactor error recorder

--- employee-service/src/main/java/com/employee/config/feign/MessageErrorDecoder.java
+++ employee-service/src/main/java/com/employee/config/feign/MessageErrorDecoder.java
@@ -4,24 +4,25 @@ import com.employee.config.ApplicationExceptionHandler;
 import com.employee.exceptions.EmployeeServiceConsumerException;
 import feign.Response;
 import feign.codec.ErrorDecoder;
+import java.util.Collection;
 import java.util.Optional;
+import java.util.function.Predicate;
 import org.springframework.http.HttpStatus;
-import org.springframework.util.CollectionUtils;
 
 public class MessageErrorDecoder implements ErrorDecoder {
 
-  private ErrorDecoder errorDecoder = new Default();
+  private final ErrorDecoder errorDecoder = new Default();
 
   @Override
   public Exception decode(String methodKey, Response response) {
-    var errors = response.headers().get(ApplicationExceptionHandler.RESPONSE_ENTITY_ERROR_HEADER);
-    Optional<String> error =
-        CollectionUtils.isEmpty(errors) ? Optional.empty() : Optional.of(String.join(",", errors));
+    String error = Optional.ofNullable(response.headers().get(ApplicationExceptionHandler.RESPONSE_ENTITY_ERROR_HEADER))
+            .filter(Predicate.not(Collection::isEmpty))
+            .flatMap(errors -> Optional.of(String.join(",", errors)))
+            .orElse("Error while calling kafka producer, please try again later.");
 
-    if (HttpStatus.valueOf(response.status()).is4xxClientError()) {
-      return new EmployeeServiceConsumerException(
-          HttpStatus.valueOf(response.status()),
-          error.orElse("Error while calling kafka producer, please try again later."));
+    var status = HttpStatus.valueOf(response.status()); 
+    if (status.is4xxClientError() || status.is5xxServerError()) {
+      return new EmployeeServiceConsumerException(status, error);
     } else {
       return errorDecoder.decode(methodKey, response);
     }
